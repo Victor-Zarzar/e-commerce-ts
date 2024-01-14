64d377daffb6c3c0b80904a0bb751ddf566c5a23
victorzarzar <victorzarzar58@gmail.com>
2024-01-14 17:48:15 -0300

    create ProductType, and new favicon, and updates in navbar and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,39 @@
 import { Metadata } from "next";
+import { ProductType } from "./types/Products";
 
 export const metadata: Metadata = {
-  title: 'KeyMart - E-Commerce',
+  title: 'KeyMart E-Commerce',
   description: 'KeyMart E-Commerce - NextJS',
 };
 
-export default function App() {
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json();
+}
+
+export default async function App() {
+
+  const products = await getProducts();
 
   return (
-    <main>
+    <>
       <link rel="shortcut icon" href="./favicon.ico"></link>
-    </main>
+      <div className="max-w-7xl mx-auto mt-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+          {products.map((product: ProductType) => {
+            <div key={product.id}>{product.title}
+
+            </div>
+          }
+          )}
+
+        </div>
+
+
+      </div>
+    </>
   )
 }
