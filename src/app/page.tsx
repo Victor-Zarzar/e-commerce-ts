b627261a26964333d021dd09286243be338f67eb
victorzarzar <victorzarzar58@gmail.com>
2024-01-15 10:57:00 -0300

    corrections in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,25 @@ import { ProductType } from "../app/types/ProductType";
 import Product from "./components/Product";
 
 async function getProducts() {
-  const res = await fetch('https://fakestoreapi.com/products')
+  const res = await fetch('https://fakestoreapi.com/products');
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
   return res.json();
 }
 
 export default async function App() {
-
   const products = await getProducts();
 
   return (
     <>
       <div className="max-w-7xl mx-auto mt-20">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-          {products.map((product: ProductType) => {
-            <Product key={product.id} product={product}>
-
-            </Product>
-          }
-          )}
+          {products.map((product: ProductType) => (
+            <Product key={product.id} product={product} />
+          ))}
         </div>
       </div>
     </>
-  )
+  );
 }
