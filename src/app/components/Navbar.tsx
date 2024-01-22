8d36b188997e83eb801646d1e792537089918e6e
victorzarzar <victorzarzar58@gmail.com>
2024-01-22 07:40:25 -0300

    updates in navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,14 +6,17 @@ import { LuShoppingCart } from "react-icons/lu";
 
 export default function Navbar() {
 
-   // const useStore = useCartStore();
+    // const useStore = useCartStore();
 
     return (
         <nav className='fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-400'>
             <Link href={'/'} className='uppercase font-bold text-sm h-10 flex items center mt-4'>KeyMart
             </Link>
-            <div className='flex items-center gap-8'>
-            <LuShoppingCart  className='h-6 w-6'/>
+            <div className='flex items-center gap-8 relative'>
+                <div className='flex gap-2'>
+                    <LuShoppingCart className='h-6 w-6' />
+                    <span className='bg-slate-600 text-sm rounded-full h-5 w-5 flex items-center justify-center absolute left-5 bottom-3'>2</span>
+                </div>
                 <SignedIn>
                     <UserButton />
                 </SignedIn>
