2432a049596bd05dd427066150359042f8fec3f7
victorzarzar <victorzarzar58@gmail.com>
2024-01-25 14:23:23 -0300

    updates in cart.tsx

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -8,11 +8,19 @@ export default function Cart() {
     const useStore = useCartStore();
 
     return (
-        <div>
+        <div onClick={() => useStore.toggleCart()} className='container'>
             <div className='flex gap-2'>
                 <LuShoppingCart className='h-6 w-6' />
                 <span className='bg-slate-600 text-sm rounded-full h-5 w-5 flex items-center justify-center absolute left-5 bottom-3'>2</span>
             </div>
+            {!useStore.isOpen && (
+                <div className='fixed w-full h-screen bg-black/25 lef-0 top-0 z-50'>
+                    <div className='absolute bg-slate-600 w-1/3 h-screen p-12 overflow-y-scroll'>
+                        <h1 className='right-0 top-0'>My Cart</h1>
+                    </div>
+                </div>
+            )
+            }
         </div>
     )
 }
